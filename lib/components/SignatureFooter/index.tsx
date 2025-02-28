4809ca7def26e860cc630bc5e4332396eba8d06a
Ethan <eth22mer@gmail.com>
2025-02-28 08:36:01 -0500

    fix icons

--- lib/components/SignatureFooter/index.tsx
+++ lib/components/SignatureFooter/index.tsx
@@ -15,20 +15,20 @@ export function SignatureFooter(props: SignatureFooterProps) {
     const { backgroundColor, fontColor } = props;
 
     const sites: siteLinks[] = [
-        { siteName: "JamSort", siteLink: "https://jamsort.com" },
-        { siteName: "What Genre is This", siteLink: "https://whatgenreisthis.com" },
-        { siteName: "The Recipes Only", siteLink: "https://therecipesonly.com" }
+        { siteName: "JamSort", siteLink: "https://www.jamsort.com" },
+        { siteName: "What Genre is This", siteLink: "https://www.whatgenreisthis.com" },
+        { siteName: "The Recipes Only", siteLink: "https://www.therecipesonly.com" }
     ];
 
     return (
         <>
             <div style={{
-                backgroundColor: backgroundColor, color: fontColor, padding: "0rem 1rem", textAlign: "center", fontSize: ".8rem"
+                backgroundColor: backgroundColor, color: fontColor, padding: "0rem 1rem", textAlign: "center", fontSize: ".8rem",
             }}>
-                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap' }}>
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: 'row', flexWrap: 'wrap', height: '35px' }}>
                     <div className='made-by'>
                         <p className="signature-text"> Made with ♡ by
-                            <a href='https://ethanmerrill.com' style={{ textDecoration: 'none' }} > Ethan Merrill </a><br></br>in Boston, Massachusetts USA</p>
+                            <a href='https://www.ethanmerrill.com' style={{ textDecoration: 'none' }} > <u>Ethan Merrill</u> </a>in Boston, Massachusetts USA</p>
                     </div>
 
                     <div>
@@ -41,18 +41,20 @@ export function SignatureFooter(props: SignatureFooterProps) {
                             </li>))}
                     </div>
                     <div style={{ display: 'flex', gap: '1rem' }} className='socials'>
-                        <div style={{ width: '24px', height: '24px' }}>
-                            <a href='https://bsky.app/profile/ethanmerrill.bsky.social'>
+                        <a style={{ height: '100%' }} href='https://bsky.app/profile/ethanmerrill.bsky.social'>
+                            <div style={{ width: '24px', height: '24px' }}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 530" width="100%" height="100%" version="1.1">
                                     <path d="m135.72 44.03c66.496 49.921 138.02 151.14 164.28 205.46 26.262-54.316 97.782-155.54 164.28-205.46 47.98-36.021 125.72-63.892 125.72 24.795 0 17.712-10.155 148.79-16.111 170.07-20.703 73.984-96.144 92.854-163.25 81.433 117.3 19.964 147.14 86.092 82.697 152.22-122.39 125.59-175.91-31.511-189.63-71.766-2.514-7.3797-3.6904-10.832-3.7077-7.8964-0.0174-2.9357-1.1937 0.51669-3.7077 7.8964-13.714 40.255-67.233 197.36-189.63 71.766-64.444-66.128-34.605-132.26 82.697-152.22-67.108 11.421-142.55-7.4491-163.25-81.433-5.9562-21.282-16.111-152.36-16.111-170.07 0-88.687 77.742-60.816 125.72-24.795z" fill={fontColor} />
                                 </svg>
-                            </a>
-                        </div>
-                        <div style={{ width: '24px', height: '24px' }}>
-                            <a href='https://www.linkedin.com/in/ethanmerrill/'>
-                                <svg xmlns="http://www.w3.org/2000/svg" height="100%" viewBox="0 0 72 72" width="100%"><g fill="none" fillRule="evenodd"><path d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z" stroke={"none"} /><path d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z" fill={fontColor} /></g></svg>
-                            </a>
-                        </div>
+                            </div>
+                        </a>
+                        <a style={{ height: '24px' }} href='https://www.linkedin.com/in/ethanmerrill/'>
+                            <div style={{ width: '24px', height: '24px' }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100%" height="100%" viewBox="0 0 50 50">
+                                    <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z" fill={fontColor} ></path>
+                                </svg>
+                            </div>
+                        </a>
                     </div>
                 </div >
             </div >
@@ -60,11 +62,13 @@ export function SignatureFooter(props: SignatureFooterProps) {
                 {`
                     .signature-text {
                         text-align: left;
-                       
+                       font-size: .7rem;
                     }
                     @media (max-width: 450px) {
                         .signature-text {
                             text-align: center;
+                            font-size: .7rem;
+                            margin: 0 auto;
                         }
                             .made-by {
                             width: 100%;
--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <SignatureFooter backgroundColor="black" fontColor="white" />
+      <SignatureFooter backgroundColor="" fontColor="" />
     </>
   )
 }
